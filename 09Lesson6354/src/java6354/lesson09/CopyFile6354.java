aa0464800fc66b67221d9fb3e0b58056fff32dd6
ybz <1615498405@qq.com>
2021-05-12 14:17:48 +0800

    modified

--- 09Lesson6354/src/java6354/lesson09/CopyFile6354.java
+++ 09Lesson6354/src/java6354/lesson09/CopyFile6354.java
@@ -1,49 +1,79 @@
 package java6354.lesson09;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.Scanner;
-
 
 /**
  * @author yeah
  */
 public class CopyFile6354 {
-  public static void main(String[] args) throws Exception {
-    Scanner input = new Scanner(System.in);
-    System.out.print("请输入要拷贝的源文件名：");
-    String source = input.nextLine();
-    System.out.print("请输入目标文件名：");
-    String target = input.nextLine();
-    //调用checkFileXXXX()方法检查两个文件名是否可用
-    //调用System.currentTimeMillis()记下开始时间-->beginTime
-    //如果两个文件名可用，调用copyXXXX()方法完成文件拷贝，并返回文件的字节数
-    //调用System.currentTimeMillis()得到当前时间，计算拷贝文件所用的时长
-    //输出拷贝文件的字节数和用时
-  }
+    public static void main(String[] args)  {
+        Scanner input = new Scanner(System.in);
+        System.out.print("请输入要拷贝的源文件名：");
+        String source = input.nextLine();
+        System.out.print("请输入目标文件名：");
+        String target = input.nextLine();
+        //调用checkFileXXXX()方法检查两个文件名是否可用
+        //如果两个文件名可用，调用copyXXXX()方法完成文件拷贝，并返回文件的字节数
+        if (checkFile6354(source, target)) {
+            //调用System.currentTimeMillis()记下开始时间-->beginTime
+            long beginTime = System.currentTimeMillis();
+            long size = copy6354(source, target);
+            //调用System.currentTimeMillis()得到当前时间，计算拷贝文件所用的时长
+            long endTime = System.currentTimeMillis();
+            long cost = endTime - beginTime;
+            //输出拷贝文件的字节数和用时
+            System.out.println("拷贝" + size + "字节文件");
+            System.out.println("用时" + cost + "ms");
+            return;
+        }
+        System.out.println("检查源文件是否存在或目标地址是否合法");
 
-  private static boolean checkFileXXXX(String sourceName, String targetName) {
-    //确保源文件已经存在
-    //调用File的getParentFile()，得到目标文件名所包含的路径
-    //判断目标路径是否已经存在，如果不存在，创建
-    return true;
+    }
 
-  }
+    private static boolean checkFile6354(String sourceName, String targetName) {
+        //确保源文件已经存在
+        if (!new File(sourceName).exists()) {
+            return false;
+        }
+        //调用File的getParentFile()，得到目标文件名所包含的路径
+        File file = new File(targetName);
+        if (!file.exists()) {
+            if (!file.getParentFile().exists()) {
+                if (!file.getParentFile().mkdirs()) {
+                    return false;
+                }
+            }
+        }
+        //判断目标路径是否已经存在，如果不存在，创建
+        return true;
 
-  private static long copyXXXX(String sourceName, String targetName) {
-    //针对源文件，创建带缓冲的输入流
-    //针对目标文件，创建带缓冲的输出流
-    // 定义一个字节数组，作为缓冲区-->buff
-    //循环执行下列步骤，直到文件结束
-    //1）从输入流中读取数据存入buff
-    //2）再把buff中的内容写入到输出流
-    //3）累加读到的字节数-->size
-    //关闭输入流和输出流
-    //返回字节数
-    return 0;
+    }
 
-  }
+    private static long copy6354(String sourceName, String targetName) {
+        try ( //针对源文件，创建带缓冲的输入流
+              BufferedInputStream bis = new BufferedInputStream(new FileInputStream(sourceName));
+              //针对目标文件，创建带缓冲的输出流
+              BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(targetName))) {
+
+            // 定义一个字节数组，作为缓冲区-->buff
+            byte[] buff = new byte[1024];
+            //循环执行下列步骤，直到文件结束
+            int len = 0, size = 0;
+            //1）从输入流中读取数据存入buff
+            while ((len = bis.read(buff)) != -1) {
+                //2）再把buff中的内容写入到输出流
+                bos.write(buff, 0, len);
+                //3）累加读到的字节数-->size
+                size += len;
+            }
+            return size;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+      return -1;
+    }
+
+
 }
--- 09Lesson6354/src/java6354/lesson09/WriteFile6354.java
+++ 09Lesson6354/src/java6354/lesson09/WriteFile6354.java
@@ -4,19 +4,18 @@ import java.io.*;
 import java.time.LocalDateTime;
 import java.util.Scanner;
 import java.util.zip.InflaterInputStream;
-
 /**
  * @author yeah
  */
 public class WriteFile6354 {
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("请输入文件名：");
         String filename = input.nextLine();
-        // 调用writeXXXX()向文件中输出字符串，并返回字节数
         System.out.println("请逐行输入数据，以end结束");
+        // 调用writeXXXX()向文件中输出字符串，并返回字节数
         // 在控制台上输出向文件中写入的字节数
+        System.out.println("文件大小为"+write6354(filename)+"B");
     }
 
     private static long write6354(String filename) {
@@ -24,33 +23,40 @@ public class WriteFile6354 {
         // 步骤1：确定输出的文件（目的地）
         File file = new File(filename);
         // 如果filename中包含路径，必须确保路径已存在
-        if (!file.exists()) {
-            System.out.println("当前路径不存在");
-            return 0;
+        if (filename.contains("\\")) {
+            if (!file.getParentFile().exists()) {
+                if (!file.getParentFile().mkdirs()) {
+                    System.out.println("路径不存在");
+                    return -1;
+                }
+            }
         }
-
         FileWriter fw = null;
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try {
             // 步骤2：创建指向文件的字符输出流
             fw = new FileWriter(file, false);
+            String data;
             // 步骤3：写数据
-            br.readLine();
-            // 输入一行写一行
+            while (!"end".equalsIgnoreCase(data = br.readLine())) {
+                // 输入一行写一行
+                fw.write(data);
+                fw.append("\n");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             // 步骤4：关闭
-            if(fw!=null) {
+            if (fw != null) {
                 try {
                     fw.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-            // 返回文件的字节数
-            return file.length();
         }
+        // 返回文件的字节数
+        return file.length();
     }
 
 }
