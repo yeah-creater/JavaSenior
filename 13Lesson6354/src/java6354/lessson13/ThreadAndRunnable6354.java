662258e2e2a270008179d6cb5571c8a528b27955
ybz <1615498405@qq.com>
2021-06-07 23:43:18 +0800

    modified

---
+++ 13Lesson6354/src/java6354/lessson13/ThreadAndRunnable6354.java
@@ -0,0 +1,14 @@
+package java6354.lessson13;
+
+/**
+ * @author: yeah
+ */
+public class ThreadAndRunnable6354 {
+    static void saleByThread6354(){
+
+    }
+    public static void main(String[] args) {
+
+
+    }
+}
